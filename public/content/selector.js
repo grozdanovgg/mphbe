557e3270c119c2088b446c77989d1a9193788a77
grozdanovgg <grozdanovgg@gmail.com>
2018-06-26 01:29:07 +0300

    Public extention folder structure. Content, background and popup comunic

---
+++ public/content/selector.js
@@ -0,0 +1,66 @@
+// const pools = [
+//     {
+//         name: 'omegapool',
+//         blocksUrl: 'https://omegapool.cc/index.php?coin=raven&page=blocks',
+//         blochsHtmlSelector: '#main > div > div > table > tbody > tr:nth-child(2) > td:nth-child(1) > a',
+
+//     }
+// ]
+
+
+// console.log('HEEEEEEEEEEEY');
+// const lastBlock = $(pool.blochsHtmlSelector);
+// console.log(lastBlock);
+// console.log('END')
+
+console.log('IN SELECTOR');
+$('body').mousemove(function (event) {
+    // console.log(event)
+    $(".mphce-hover").removeClass("mphce-hover");
+    $(event.target).addClass("mphce-hover");
+});
+
+$(document).click(function (event) {
+    event.preventDefault();
+
+    const path = fullPath(event.target);
+    var text = $(path).text();
+    console.log(text);
+
+    chrome.runtime.sendMessage({
+        type: "selected-element",
+        elementSelector: path,
+        elementText: +text
+    }, function () {
+        $(document).unbind('click');
+        $('body').unbind('mousemove');
+        $(".mphce-hover").removeClass("mphce-hover");
+    });
+});
+
+
+
+
+
+
+
+
+
+function fullPath(el) {
+    var names = [];
+    while (el.parentNode) {
+        if (el.id) {
+            names.unshift('#' + el.id);
+            break;
+        } else {
+            if (el == el.ownerDocument.documentElement) names.unshift(el.tagName);
+            else {
+                for (var c = 1, e = el; e.previousElementSibling; e = e.previousElementSibling, c++);
+                names.unshift(el.tagName + ":nth-child(" + c + ")");
+            }
+            el = el.parentNode;
+        }
+    }
+    return names.join(" > ");
+}
+
--- public/popup.js
+++ public/popup.js
@@ -1,51 +1,51 @@
-const user = {
-    name: "pesho"
-}
+// let changeColor = document.getElementById('changeColor');
 
-///////
-
-let changeColor = document.getElementById('changeColor');
-
-chrome.storage.sync.get('color', function (data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-});
+// chrome.storage.sync.get('color', function (data) {
+//     changeColor.style.backgroundColor = data.color;
+//     changeColor.setAttribute('value', data.color);
+// });
 
 changeColor.onclick = function (element) {
-    let color = element.target.value;
+    // let color = element.target.value;
+    console.log('clicked');
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            { code: `document.body.style.backgroundColor = "${color}";` });
+        // chrome.tabs.executeScript(
+        //     tabs[0].id,
+        //     { code: `document.body.style.backgroundColor = "${color}";` });
+        console.log(tabs);
+        chrome.tabs.executeScript({
+            file: 'content/selector.js'
+        });
     });
 };
 
-const table = document.getElementById("table-body");
 
-const row = table.insertRow(0);
 
-var cell1 = row.insertCell(0);
-var cell2 = row.insertCell(1);
+
+// const table = document.getElementById("table-body");
+
+// const row = table.insertRow(0);
+
+// var cell1 = row.insertCell(0);
+// var cell2 = row.insertCell(1);
+
 
 // Add some text to the new cells:
-cell1.innerHTML = "NEW CELL1";
-cell2.innerHTML = "NEW CELL2";
-
-
+// cell1.innerHTML = "NEW CELL1";
+// cell2.innerHTML = "NEW CELL2";
 
 // Add pool 
 
+// const addPoolBtn = document.getElementById("add-pool");
+// addPoolBtn.onclick = function (element) {
 
-const addPoolBtn = document.getElementById("add-pool");
-addPoolBtn.onclick = function (element) {
+// }
 
-}
-
-const blockWatchBtn = document.getElementById("block-to-watch");
-blockWatchBtn.onclick = function (element) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            { code: `document.body.style.backgroundColor = "${color}";` });
-    });
-}
+// const blockWatchBtn = document.getElementById("block-to-watch");
+// blockWatchBtn.onclick = function (element) {
+//     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//         chrome.tabs.executeScript(
+//             tabs[0].id,
+//             { code: `document.body.style.backgroundColor = "${color}";` });
+//     });
+// }
